420cb038d2db93053b85a6c1f1319f395141a4ab
Alex <vassilyevalexey@gmail.com>
2018-05-04 14:21:13 +0600

    999

--- HW Module 9.2/Program.cs
+++ HW Module 9.2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HW_Module_9._2.Module;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace HW_Module_9._2
     {
         static void Main(string[] args)
         {
-            
+           
         }
     }
 }
